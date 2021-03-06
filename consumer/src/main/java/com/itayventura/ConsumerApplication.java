85c7eafb801a7745c5e8ef3db7d04c04722d5b5b
itayventura <ventura.itay@gmail.com>
2021-03-06 10:35:27 +0200

    adding a tasked time. adding an argument hello....

--- consumer/src/main/java/com/itayventura/ConsumerApplication.java
+++ consumer/src/main/java/com/itayventura/ConsumerApplication.java
@@ -1,39 +1,15 @@
 package com.itayventura;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsumerApplication {
 
-	private final static String QUEUE_NAME = "hello";
-	private final static Logger LOGGER = LoggerFactory.getLogger(ConsumerApplication.class);
-
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ConsumerApplication.class, args);
-		consume();
+		Worker.consume();
 	}
 
-	private static void consume() throws Exception{
-		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
-		Connection connection = factory.newConnection();
-		Channel channel = connection.createChannel();
-
-		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-		LOGGER.info(" [*] Waiting for messages. To exit press CTRL+C");
-
-		DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-			String message = new String(delivery.getBody(), "UTF-8");
-			LOGGER.info(" [x] Received '" + message + "'");
-		};
-		channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
-	}
 
 }
---
+++ consumer/src/main/java/com/itayventura/Worker.java
@@ -0,0 +1,52 @@
+package com.itayventura;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Worker {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+    private final static Logger LOGGER = LoggerFactory.getLogger(Worker.class);
+
+
+    public static void consume() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        LOGGER.info(" [*] Waiting for messages. To exit press CTRL+C");
+
+        channel.basicQos(1);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+
+            LOGGER.info(" [x] Received '" + message + "'");
+            try {
+                doWork(message);
+            } finally {
+                LOGGER.info(" [x] Done");
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
+        };
+        channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> { });
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
