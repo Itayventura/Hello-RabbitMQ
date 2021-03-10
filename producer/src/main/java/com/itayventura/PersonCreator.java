f37ee5d0d59dc34c11d06b1b8bcdc36f22bbd924
itayventura <ventura.itay@gmail.com>
2021-03-10 22:44:55 +0200

    adding a dockerfile to producer

--- producer/src/main/java/com/itayventura/PersonCreator.java
+++ producer/src/main/java/com/itayventura/PersonCreator.java
@@ -8,7 +8,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
