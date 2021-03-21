83a344d8de6d06a6ddc32a7811c0df8d113779f5
itayventura <ventura.itay@gmail.com>
2021-03-21 16:30:55 +0200

    added a scheduled message - every 2 seconds

--- producer/src/main/java/com/itayventura/PersonCreator.java
+++ producer/src/main/java/com/itayventura/PersonCreator.java
@@ -8,6 +8,8 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
+@EnableScheduling
 public class PersonCreator implements CommandLineRunner {
     @Value("${amqp.queue.name}")
     private String queueName;
@@ -30,6 +33,19 @@ public class PersonCreator implements CommandLineRunner {
         this.rabbitTemplate = rabbitTemplate;
         this.objectMapper = objectMapper;
     }
+
+    @Scheduled(fixedRate = 2000L)
+    public void sendMessage(){
+        try {
+            String jsonString = objectMapper.writeValueAsString(new Person("Scheduled", "Person"));
+            rabbitTemplate.convertAndSend(queueName, jsonString);
+            LOGGER.info(" [x] Sent " + jsonString);
+        } catch (JsonProcessingException e) {
+            LOGGER.error("Parsing Exception", e);
+        }
+    }
+
+
     @Override
     public void run(String... args) {
         List<Person> persons = new ArrayList<>(
